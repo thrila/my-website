5fc4e429a5650b60d8743327ff3b204399d6b1ab
Thrila <Ifeanyindubizu503@gmail.com>
2023-10-24 21:50:56 +0100

    fix: blogItem Bold

--- src/app/components/BlogSection/blogItem.tsx
+++ src/app/components/BlogSection/blogItem.tsx
@@ -15,7 +15,7 @@ export const BlogItem = ({ data }: Props) => {
       <Link href={`/blog/${id}`} >
         <div className='transition flex md:flex-row flex-col justify-between md:space-x-20 py-3 border-b border-zinc-400 font-thin ease-in duration-500 hover:bg-button-before hover:text-custom-background'>
           <div className='pr-2 w-1/8 pl-2'><p className=' font-thin text-sm'>{formattedDate}</p></div>
-          <div className='flex justify-start'><h3 className='text-left text-sm md:text-xl'>{formattedTitle} </h3></div>
+          <div className='flex justify-start'><h3 className='text-left text-sm font-bold md:text-xl'>{formattedTitle} </h3></div>
           <div className='pr-2 w-1/8 pl-2'><p className=' text-sm'>{id}</p>
           </div>
         </div>
