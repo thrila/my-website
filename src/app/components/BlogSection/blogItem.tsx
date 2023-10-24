4dff0184b1971646d79ed0f85ff0ead5e99c5ace
Thrila <Ifeanyindubizu503@gmail.com>
2023-10-24 21:46:55 +0100

    fix: blogItem

--- src/app/components/BlogSection/blogItem.tsx
+++ src/app/components/BlogSection/blogItem.tsx
@@ -13,10 +13,10 @@ export const BlogItem = ({ data }: Props) => {
   return (
     <div >
       <Link href={`/blog/${id}`} >
-        <div className='transition flex md:flex-row flex-col justify-between md:space-x-20 py-3 border-b border-zinc-400 font-thin ease-in duration-150 hover:bg-hover-color'>
-          <div className='pr-2 w-1/8 pl-2'><p className='text-button-before font-thin text-sm'>{formattedDate}</p></div>
+        <div className='transition flex md:flex-row flex-col justify-between md:space-x-20 py-3 border-b border-zinc-400 font-thin ease-in duration-500 hover:bg-button-before hover:text-custom-background'>
+          <div className='pr-2 w-1/8 pl-2'><p className=' font-thin text-sm'>{formattedDate}</p></div>
           <div className='flex justify-start'><h3 className='text-left text-sm md:text-xl'>{formattedTitle} </h3></div>
-          <div className='pr-2 w-1/8 pl-2'><p className='text-button-before text-sm'>{id}</p>
+          <div className='pr-2 w-1/8 pl-2'><p className=' text-sm'>{id}</p>
           </div>
         </div>
       </Link>
