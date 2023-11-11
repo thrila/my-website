2c469f76d5cd3f65e417d895a6408dfc389e161b
Thrila <Ifeanyindubizu503@gmail.com>
2023-11-11 21:51:25 +0100

    chore:change twitter icon

--- src/app/components/iconSection/socials.tsx
+++ src/app/components/iconSection/socials.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { SlSocialGithub, SlSocialTwitter, SlSocialLinkedin } from 'react-icons/sl'
 import { SiDiscord, SiHackernoon, SiHackerrank } from 'react-icons/si'
+import { RiTwitterXFill } from 'react-icons/ri'
 
 
 const Socials = () => {
   return (
     <div className=' mx-auto'>
       <div className=' flex items-center space-x-5 md:space-x-10 m-3'>
-        <a target="_blank" rel="noopener noreferrer" href='https://www.github.com/thrila' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before'aria-label="Github"> <SlSocialGithub /> </a>
+        <a target="_blank" rel="noopener noreferrer" href='https://www.github.com/thrila' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label="Github"> <SlSocialGithub /> </a>
         <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/michael-ndubuizu-3a49a31b7/' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label='linkedin'> <SlSocialLinkedin /> </a>
-        <a target="_blank" rel="noopener noreferrer" href='https://twitter.com/nino_da_creator' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label='twitter'> <SlSocialTwitter /> </a>
+        <a target="_blank" rel="noopener noreferrer" href='https://twitter.com/nino_da_creator' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label='x'> <RiTwitterXFill /> </a>
         <a target="_blank" rel="noopener noreferrer" href='discordapp.com/users/830840937273229332' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label='discord'> <SiDiscord /> </a>
         <a target="_blank" rel="noopener noreferrer" href='https://hackernoon.com/u/indieboy' className='text-white text-4xl md:text-6xl font-thin hover:text-button-before' aria-label='hackernoon'> <SiHackernoon /> </a>
       </div>
