1cba079b769f1939e53c34c8fd2925af229a8962
Thrila <Ifeanyindubizu503@gmail.com>
2024-07-14 15:04:59 +0100

    add - rust project

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -4,6 +4,13 @@ const ProjectItems: ProjectItem[] = [
     technologies: "JavaScript",
     description: 'a simple cli tool to check about movies info',
     website: 'https://github.com/thrila/about-movie'
+  },
+
+  {
+    name: "Rot",
+    technologies: "Rust",
+    description: 'a cli application for encrpyting plane text files ',
+    website: 'https://github.com/thrila/rot'
   }
 ]
 
