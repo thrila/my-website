d5794e8cd174278a2f68b60510237f6af2581aa8
Thrila <Ifeanyindubizu503@gmail.com>
2024-09-23 12:28:35 +0100

    fix style

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className='my-6 container max-w-3xl'>
+    <div className="my-6 container max-w-3xl">
       {/* hero div  */}
-      < div className='flex justify-around md:justify-between flex-col-reverse md:flex-row ' >
-        <div className='pr-auto md:w-1/2'>
-          <div className=' my-3'>
-            <h1 className='font-bold font-serif text-3xl md:text-5xl text-custom-text'>Michael Emmanuel</h1>
+      <div className="flex justify-around md:justify-between flex-col-reverse md:flex-row ">
+        <div className="pr-auto md:w-1/2">
+          <div className=" my-3">
+            <h1 className="font-bold font-serif text-3xl md:text-5xl text-custom-text">
+              Michael Emmanuel
+            </h1>
           </div>
-          <div className='w-auto md:pr-5'>
-            <p className=' text-left font-normal text-custom-text'> Student of Engineering, <Link className='text-text-light' href='www.github.com/thrila'>Web Developer</Link>, Linux and Open Source enthusiast with 3+ years of experience making web apps and writing hacky scripts</p>
+          <div className="w-auto md:pr-5">
+            <p className=" text-left font-normal text-custom-text">
+              {" "}
+              Student of Engineering,{" "}
+              <Link className="text-text-light" href="www.github.com/thrila">
+                Web Developer
+              </Link>
+              , Linux and Open Source enthusiast with 3+ years of experience making web apps and
+              writing hacky scripts
+            </p>
           </div>
         </div>
-        <div className=' pl-auto rounded-full h-20 w-20 border-2 border-white  md:h-40 md:w-40 relative '>
-          <Image className='object-cover w-full h-full overflow-hidden rounded-full'
-            alt='Danny phanthom'
-            src='/danny.jpeg'
-            layout='fill'
-            loading='eager'
-
+        <div className=" pl-auto rounded-full h-20 w-20 border-2 border-white  md:h-40 md:w-40 relative ">
+          <Image
+            className="object-cover w-full h-full overflow-hidden rounded-full"
+            alt="Danny phanthom"
+            src="/danny.jpeg"
+            layout="fill"
+            fill={true}
+            loading="eager"
           />
         </div>
-      </div >
-    </div >
-  )
-}
+      </div>
+    </div>
+  );
+};
--- src/app/components/projectSection/projectCard.tsx
+++ src/app/components/projectSection/projectCard.tsx
@@ -1,21 +1,39 @@
-import Link from 'next/link'
+import Link from "next/link";
 import { FaCode } from "react-icons/fa";
 import { TbWorldWww } from "react-icons/tb";
 
 const Card = ({ name, technologies, description, website }: ProjectItem) => {
   return (
-    <div className='container max-w-xl py-4 rounded bg-hover-color'>
-      <Link href='#' className='flex flex-col justify-center items-left '>
-        <h1 className='font-bold text-2xl pb-1 font-serif'>{name}</h1>
-        <p className='text-custom-background pb-2 font-mono'>{technologies} </p>
-        <p className='font-normal text font-sans'>{description}</p>
-        <div className='flex space-x-5 py-2 text-xl font-thin'>
-          <a href={website} target='_blank' rel="noopener noreferrer" aria-label='Projects Source code'> <FaCode /> </a>
-          <a href='#' target='_blank' rel="noopener noreferrer" aria-label='Projects Live demo'> <TbWorldWww /> </a>
+    <div className="container max-w-xl py-4 rounded bg-hover-color">
+      <Link href="#" className="flex flex-col justify-center items-left ">
+        <h1 className="font-bold text-white text-2xl pb-1 font-serif">{name}</h1>
+        <p className="text-custom-background  pb-2 font-mono">{technologies} </p>
+        <p className="font-normal text font-sans text-white">{description}</p>
+        <div className="flex space-x-5 py-2 text-xl font-thin">
+          <a
+            href={website}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Projects Source code"
+            className="text-white"
+          >
+            {" "}
+            <FaCode />{" "}
+          </a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Projects Live demo"
+            className="text-white"
+          >
+            {" "}
+            <TbWorldWww />{" "}
+          </a>
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
